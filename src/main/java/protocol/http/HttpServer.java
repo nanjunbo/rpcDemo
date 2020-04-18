f1dbdbac3209c79b8db2d2bc1451999e5a42cd2d
nanjunbo <345855983@qq.com>
2020-04-18 10:22:20 +0800

    add

--- src/main/java/protocol/http/HttpServer.java
+++ src/main/java/protocol/http/HttpServer.java
@@ -1,8 +1,10 @@
 package protocol.http;
 
-import org.apache.catalina.Server;
-import org.apache.catalina.Service;
+import org.apache.catalina.*;
 import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardEngine;
+import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.startup.Tomcat;
 
 public class HttpServer {
@@ -13,5 +15,34 @@ public class HttpServer {
         Service service = server.findService("Tomcat");
 
         Connector connector = new Connector();
+        connector.setPort(port);
+
+        Engine engine = new StandardEngine();
+        engine.setDefaultHost(hostName);
+
+        Host host = new StandardHost();
+        host.setName(hostName);
+
+        String contextPath = "";
+        Context context = new StandardContext();
+        context.setPath(contextPath);
+        context.addLifecycleListener(new Tomcat.FixContextListener());
+
+        host.addChild(context);
+        engine.addChild(context);
+
+        service.setContainer(engine);
+        service.addConnector(connector);
+
+        tomcat.addServlet(contextPath, "dispatcher", new DispatcherServlet());
+        context.addServletMappingDecoded("/*" ,"dispatcher");
+
+        try {
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
+
     }
 }
